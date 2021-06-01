2e5ab3b9d9df38d63c2f91667a923fe3f492db3d
Jefferson <jeffersongarcia1599@gmail.com>
2021-06-01 01:22:42 -0500

    done

--- double_linked_list.py
+++ double_linked_list.py
@@ -12,87 +12,79 @@ class DoublyLinkedList:
         self.tail = None
 
     def setHead(self, node):
-        # Write your code here.
         if self.head is None:
-		    self.head = node
-			self.tail = node
-		else:
-			self.insertBefore(self.head, node)
-
+            self.head = node
+            self.tail = node
+        else:
+            self.insertBefore(self.head, node)
+    
     def setTail(self, node):
-        # Write your code here.
         if self.tail is None:
-			self.setHead(node)
-			return
-		self.insertAfter(self.tail, node)
+            self.setHead(node)
+            return
+        self.insertAfter(self.tail, node)
 
     def insertBefore(self, node, nodeToInsert):
-        # Write your code here.
         if nodeToInsert == self.head and nodeToInsert == self.tail:
-			return
-		self.remove(nodeToInsert)
-		nodeToInsert.prev = node.prev
-		nodeToInsert.next = node
-		if node.prev is None:
-			self.head = nodeToInsert
-		else:
-			node.prev.next = nodeToInsert
-		node.prev = nodeToInsert
-
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node.prev
+        nodeToInsert.next = node.next
+        if node.prev is None:
+            self.head = nodeToInsert
+        else:
+            node.prev.next = nodeToInsert
+        node.prev = nodeToInsert
+        
     def insertAfter(self, node, nodeToInsert):
-        # Write your code here.
         if nodeToInsert == self.head and nodeToInsert == self.tail:
-			return
-		self.remove(nodeToInsert)
-		nodeToInsert.prev = node
-		nodeToInsert.next = node.next
-		if node.next is None:
-			self.tail = nodeToInsert
-		else:
-			node.next.prev = nodeToInsert
-		node.next = nodeToInsert
-		
-		
-
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node
+        nodeToInsert.next = node.next
+        if node.next is None:
+            self.tail = nodeToInsert
+        else:
+            node.next.prev = nodeToInsert
+        node.next = nodeToInsert
     def insertAtPosition(self, position, nodeToInsert):
-        # Write your code here.
         if position == 1:
-			self.setHead(nodeToInsert)
-			return
-		node = self.head
-		currentPosition = 1
-		while node is not None and currentPosition != position:
-			node = node.next
-			currentPosition += 1
-		if node is not None:
-			self.insertBefore(node, nodeToInsert)
-		else:
-			self.setTail(nodeToInsert)
-
+            self.setHead(nodeToInsert)
+            return
+        node = self.head
+        currentPosition = 1
+        while node is not None and currentPosition != position:
+            node = node.next
+            currentPosition += 1
+        if node is not None:
+            self.insertBefore(node, nodeToInsert)
+        else:
+            self.setTail(nodeToInsert)
+        
     def removeNodesWithValue(self, value):
-        # Write your code here.
-		node = self.head
-		while node is not None:
-			nodeToRemove = node
-			node = node.next
-			if nodeToRemove.value == value:
-				self.remove(nodeToRemove)
+        node = self.head
+        while node is not None:
+            nodeToRemove = node
+            node = node.next
+            if nodeToRemove.value == value:
+                self.remove(nodeToRemove)
+    
+    def remove(self, node):
+        if node == self.head:
+            self.head = self.head.next
+        if node == self.tail:
+            self.tail = self.tail.prev
+        self.removeNodePointers(node)
+    
+    def containsNodeWithValue(self, value):
+        node = self.head
+        while node is not None and node.value != value:
+            node = node.next
+        return node is not None
+    
+    def remore
         
 
-    def remove(self, node):
-        # Write your code here.
-        if (node == self.head):
-			self.head = self.head.next
-		if (node == self.tail):
-			self.tail = self.tail.prev
-		self.removeNodePointers(node)
-
-    def containsNodeWithValue(self, value):
-        # Write your code here.
-        node = self.head
-		while node is not None and node.value != value:
-			node = node.next
-		return node is not None
 	
 	def removeNodePointers(self, node):
 		if node.prev is not None:
