5b98e4f6a90021b4bb9e0d9bc2eb594cac3644bf
Jefferson <jeffersongarcia1599@gmail.com>
2021-08-24 10:31:48 -0500

    Done with Data Structures problem

---
+++ data_structures/remove_duplicates_from_linkedlist.py
@@ -0,0 +1,15 @@
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+def removeDuplicatesFromLinkedList(linkedList):
+    currentNode = linkedList
+    while currentNode is not None:
+        nextDistinctNode = currentNode.next
+        while nextDistinctNode is not None and nextDistinctNode.value == currentNode.value:
+            nextDistinctNode = nextDistinctNode.next
+        currentNode.next = nextDistinctNode
+        currentNode = nextDistinctNode
+    return linkedList
+
