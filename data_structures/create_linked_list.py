f6bcd6bf217d574d96ec7dea3dc99bd9e7edf396
Jefferson <jeffersongarcia1599@gmail.com>
2021-08-27 16:08:13 -0500

    Done with Create LinkedList Problem

---
+++ data_structures/create_linked_list.py
@@ -0,0 +1,17 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+# O(n) Time | O(n) Space
+def create_linked_list(values):
+    temporary_head = Node(None)
+    tail = temporary_head
+    for new_node in values:
+        tail.next = Node(new_node)
+        tail = tail.next
+    return temporary_head.next
+
+
+print(create_linked_list(["h", "e", "y"]))
+# h -> e -> y
