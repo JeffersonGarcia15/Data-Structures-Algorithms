ed33b014d1d228650b1657eee045824fcbcb513c
Jefferson <jeffersongarcia1599@gmail.com>
2021-09-09 09:53:55 -0500

    A different solution for compress

--- Strings/compress.py
+++ Strings/compress.py
@@ -1,22 +1,42 @@
 # O(n) time | O(n) Space
 def compress(s):
-    s += '!'
-    i = 0
-    j = 0
-    result = []
+    # s += '!'
+    # i = 0
+    # j = 0
+    # result = []
     
-    while j < len(s):
-        if s[i] == s[j]:
-            j += 1
-        else:
-            num = j - i
-            if num == 1:
-                result.append(s[i])
-            else:
-                result.append(str(num))
-                result.append(s[i])
-            i = j
-    return ''.join(result)
+    # while j < len(s):
+    #     if s[i] == s[j]:
+    #         j += 1
+    #     else:
+    #         num = j - i
+    #         if num == 1:
+    #             result.append(s[i])
+    #         else:
+    #             result.append(str(num))
+    #             result.append(s[i])
+    #         i = j
+    # return ''.join(result)
+    # def compress(s):
+  s += '!'
+  left = 0
+  right = 1
+  count = 1
+  string = []
+  while right < len(s):
+    if s[left] == s[right]:
+      right += 1
+      count += 1
+    else:
+      if count == 1:
+        string.append(s[left])
+      else:
+        string.append(str(count))
+        string.append(s[left])
+      left = right
+      right += 1
+      count = 1
+  return ''.join(string)
 
 print(compress('ccaaatsss'))
     
