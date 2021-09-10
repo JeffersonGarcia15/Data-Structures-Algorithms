11ec91bccbbce9ced9637a2ae052f6ecae1ad454
Jefferson <jeffersongarcia1599@gmail.com>
2021-09-10 10:46:39 -0500

    Solved Pair Product using a while loop

--- Arrays/pair_product.py
+++ Arrays/pair_product.py
@@ -1,8 +1,18 @@
 # O(n) time | O(n) Space
 def pair_product(numbers, target_product):
+    # nums = {}
+    # for index, num in enumerate(numbers):
+    #     currentProduct = target_product // num
+    #     if currentProduct in nums:
+    #         return (nums[currentProduct], index)
+    #     nums[num] = index 
     nums = {}
-    for index, num in enumerate(numbers):
-        currentProduct = target_product // num
+    i = 0
+    count = 0
+    while i < len(numbers):
+        currentProduct = target_product // numbers[i]
         if currentProduct in nums:
-            return (nums[currentProduct], index)
-        nums[num] = index 
+            return (nums[currentProduct], count)
+        nums[numbers[i]] = count
+        i += 1
+        count += 1
