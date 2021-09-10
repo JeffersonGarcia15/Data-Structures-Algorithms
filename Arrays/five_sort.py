6542c0264409aa36604b01c29d4b5dd49105762a
Jefferson <jeffersongarcia1599@gmail.com>
2021-09-10 11:06:58 -0500

    A slightly different version of Five Sort

--- Arrays/five_sort.py
+++ Arrays/five_sort.py
@@ -1,13 +1,23 @@
 # O(n) Time | O(n) Space
 def five_sort(nums):
+    # left = 0
+    # right = len(nums) - 1
+    # while left < right:
+    #     if nums[right] == 5:
+    #         right -= 1
+    #     elif nums[left] == 5:
+    #         nums[left], nums[right] = nums[right], nums[left]
+    #         left += 1
+    #     else:
+    #         left += 1
+    # return nums
     left = 0
     right = len(nums) - 1
     while left < right:
+        if nums[left] == 5 and nums[right] != 5:
+            nums[left], nums[right] = nums[right], nums[left]
         if nums[right] == 5:
             right -= 1
-        elif nums[left] == 5:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-        else:
+        if nums[left] != 5:
             left += 1
     return nums
