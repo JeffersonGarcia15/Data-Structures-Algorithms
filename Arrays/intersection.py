ac3a0798a6f010cbd4120144ed90753721869f0a
Jefferson <jeffersongarcia1599@gmail.com>
2021-09-10 10:56:01 -0500

    Solved Intersection using a while loop

--- Arrays/intersection.py
+++ Arrays/intersection.py
@@ -3,3 +3,14 @@
 def intersection(a, b):
     set_a = set(a)
     return [ item for item in b if item in set_a ]
+
+
+# def intersection(a, b):
+#   new_set = set(a)
+#   i = 0
+#   new_array = []
+#   while i < len(b):
+#     if b[i] in new_set:
+#       new_array.append(b[i])
+#     i += 1
+#   return new_array
