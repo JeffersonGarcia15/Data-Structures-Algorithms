e195e12c16b75c84ab7e608f01e0f6d4a0dc854a
Jefferson <jeffersongarcia1599@gmail.com>
2021-09-10 10:40:55 -0500

    Solved Pair Sum using a while loop

--- Arrays/pair_sum.py
+++ Arrays/pair_sum.py
@@ -1,11 +1,21 @@
 # O(n) time | O(n) Space
 def pair_sum(numbers, target_sum):
-    nums = {}
-    for index, num in enumerate(numbers):
-        currentSum = target_sum - num
-        if currentSum in nums:
-            return (nums[currentSum], index)
-        nums[num] = index
-        print(nums)
+    # nums = {}
+    # for index, num in enumerate(numbers):
+    #     currentSum = target_sum - num
+    #     if currentSum in nums:
+    #         return (nums[currentSum], index)
+    #     nums[num] = index
+    #     print(nums)
+    dict = {}
+    count = 0
+    i = 0
+    while i < len(numbers):
+        complement = target_sum - numbers[i] # 5 6 3
+        if complement in dict:
+            return (dict[complement], count)
+        dict[numbers[i]] = count # {3:0, 2:1}
+        count += 1
+        i += 1
         
 print(pair_sum([3,2,5,4,1], 8))
