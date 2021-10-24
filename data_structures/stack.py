1f4b9e6aeecf3f2fa7dae1e7c445198520d35ea4
Jefferson <jeffersongarcia1599@gmail.com>
2021-10-24 10:02:06 -0500

    Creating Node Class

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -10,35 +10,37 @@
 class Node:
     def __init__(self, value):
         self.size = 0
-        self.first = None
-        self.last = None
+        self.value = value
+        self.next = None
+        
 
-class Stack(object):
-    def __init__(self):
-        self.items = []
+
+# class Stack(object):
+#     def __init__(self):
+#         self.items = []
         
-    def isEmpty(self):
-        return self.items == []
+#     def isEmpty(self):
+#         return self.items == []
     
-    def push(self, item):
-        self.items.append(item)
+#     def push(self, item):
+#         self.items.append(item)
         
-    def pop(self):
-        return self.items.pop()
+#     def pop(self):
+#         return self.items.pop()
     
-    def peek(self):
-        return self.items[len(self.items) - 1]
+#     def peek(self):
+#         return self.items[len(self.items) - 1]
     
-    def size(self):
-        return len(self.items)
+#     def size(self):
+#         return len(self.items)
     
-s = Stack()
-print(s.isEmpty())
-s.push(1)
-s.push('two')
-s.push(True)
-print(s.peek())
-print(s.size())
-print(s.isEmpty())
-print(s.pop())
-print(s.peek())
+# s = Stack()
+# print(s.isEmpty())
+# s.push(1)
+# s.push('two')
+# s.push(True)
+# print(s.peek())
+# print(s.size())
+# print(s.isEmpty())
+# print(s.pop())
+# print(s.peek())
--- test/stack_test.py
+++ test/stack_test.py
@@ -1,5 +1,10 @@
 import unittest
 from data_structures import stack
 
-
-
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.node = stack.Node()
+        self.stack = stack.Stack()
+        
+    
+        
