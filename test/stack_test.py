90e91c962b5f3d7c088660b15b13d78f8c67edb8
Jefferson <jeffersongarcia1599@gmail.com>
2021-10-24 10:36:07 -0500

    Checking Pop behavior

--- test/stack_test.py
+++ test/stack_test.py
@@ -9,6 +9,7 @@ class Test(unittest.TestCase):
     def tearDown(self):
         while not self.stack.size == 0:
             self.stack.pop()
+            
     def test_that_pointers_are_at_null_in_empty_stack(self):
         self.first = None
         self.last = None
@@ -27,5 +28,29 @@ class Test(unittest.TestCase):
         self.stack.push(20)
         self.first = 20
         self.last = 10
+        self.assertEqual(self.first, self.stack.first.value)
+        self.assertEqual(self.last, self.stack.last.value)
+        
+    def test_that_checks_if_can_do_pop_on_empty_stack(self):
+        self.stack.pop()
+        self.first = None
+        self.last = None
+        self.assertEqual(self.first, self.stack.first)
+        self.assertEqual(self.last, self.stack.last)
+
+    def test_that_checks_if_pop_moves_pointer_to_none_on_stack_with_a_single_value(self):
+        self.stack.push(1)
+        self.stack.pop()
+        self.first = None
+        self.last = None
+        self.assertEqual(self.first, self.stack.first)
+        self.assertEqual(self.last, self.stack.last)
+        
+    def test_that_checks_if_pop_moves_pointer_to_same_node_on_stack_with_two_values(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.pop()
+        self.first = 1
+        self.last = 1
         self.assertEqual(self.first, self.stack.first.value)
         self.assertEqual(self.last, self.stack.last.value)
